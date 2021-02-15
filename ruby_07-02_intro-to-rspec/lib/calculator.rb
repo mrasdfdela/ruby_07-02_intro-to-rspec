b475aa85d09e0f38245ba062a53ec629845d9792
jerry <jerryhsu830@gmail.com>
2021-02-15 18:10:51 -0500

    introductory calculator example

---
+++ ruby_07-02_intro-to-rspec/lib/calculator.rb
@@ -0,0 +1,19 @@
+#lib/calculator.rb
+
+class Calculator
+  def add(a, b, c=0)
+    a + b + c
+  end
+
+  def multiply(a,b)
+    a * b
+  end
+
+  def divide(a,b)
+    a / b
+  end
+
+  def subtract(a,b)
+    a - b
+  end
+end
